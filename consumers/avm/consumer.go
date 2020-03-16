466cca7856d2a541026e3d4e4fcacc1b0ec7e49c
Tyler Smith <tylersmith.me@gmail.com>
2020-03-16 13:03:05 -0700

    FEATURE: Add a consumer that writes to Redis.

--- consumers/avm/consumer.go
+++ consumers/avm/consumer.go
@@ -1,15 +1,90 @@
 package avm
 
-import "github.com/ava-labs/gecko/utils/logging"
+import (
+	"time"
+
+	"github.com/ava-labs/gecko/ids"
+	"github.com/ava-labs/gecko/utils/logging"
+	"github.com/go-redis/redis"
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+
+	"github.com/ava-labs/ortelius/cache"
+	"github.com/ava-labs/ortelius/cfg"
+)
+
+var (
+	defaultTimeout = 1 * time.Minute
+)
 
 // AVM consumes for the AVM
-type AVM struct{}
+type AVM struct {
+	log      logging.Logger
+	consumer *kafka.Consumer
+	cache    cache.Accumulator
+	topic    string
+	chainID  ids.ID
+}
 
 // Initialize inits the consumer
-func (c *AVM) Initialize(logging.Logger) error { return nil }
+func (c *AVM) Initialize(log logging.Logger, kafkaConfig kafka.ConfigMap) error {
+	var err error
+	c.log = log
+	c.topic = cfg.Viper.GetString("chainID")
+	log.Info("chainID: %s", cfg.Viper.GetString("chainID"))
+	if c.chainID, err = ids.FromString(c.topic); err != nil {
+		c.log.Error("Invalid chainID: %s", err.Error())
+		return err
+	}
+
+	if c.consumer, err = kafka.NewConsumer(&kafkaConfig); err != nil {
+		c.log.Error("Error creating consumer: %s", err.Error())
+		return err
+	}
+
+	if err = c.consumer.Subscribe(c.topic, nil); err != nil {
+		c.log.Error("Error subscribing to topic: %s", err.Error())
+		return err
+	}
+
+	// TODO: Put into config
+	c.cache, err = cache.NewRedisBackend(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	if err != nil {
+		return err
+	}
+
+	return err
+}
 
 // Close closes the consumer
-func (c *AVM) Close() {}
+func (c *AVM) Close() error {
+	return c.consumer.Close()
+}
 
-// ReadMessage consumers a message
-func (c *AVM) ReadMessage(msg []byte) error { return nil }
+// Accept waits for a new message, and if it receives one it and processes it
+func (c *AVM) Accept() error {
+	msg, err := c.consumer.ReadMessage(defaultTimeout)
+	if err != nil {
+		return err
+	}
+
+	return handleMessage(c.cache, msg)
+}
+
+// handleMessage takes in a raw message from Kafka and writes it to the cache
+func handleMessage(cacheAcc cache.Accumulator, msg *kafka.Message) error {
+	id, err := ids.ToID(msg.Key)
+	if err != nil {
+		return err
+	}
+
+	err = cacheAcc.AddTx(id, msg.Value)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- consumers/base.go
+++ consumers/base.go
@@ -2,14 +2,16 @@ package consumers
 
 import (
 	"github.com/ava-labs/gecko/utils/logging"
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+
 	"github.com/ava-labs/ortelius/consumers/avm"
 )
 
 // BaseType is a basic interface for consumers
 type BaseType interface {
-	Initialize(logging.Logger) error
-	Close()
-	ReadMessage([]byte) error
+	Initialize(logging.Logger, kafka.ConfigMap) error
+	Accept() error
+	Close() error
 }
 
 // Select chooses the correct producer based on the dataType flag
