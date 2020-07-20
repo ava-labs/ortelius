4b2d9c8cdb7664b408b158b1f0eb92b91162c8cf
Tyler Smith <tylersmith.me@gmail.com>
2020-07-19 20:42:30 -0700

    CI: Push to public docker repo.

--- .ci/deploy_images.sh
+++ .ci/deploy_images.sh
@@ -3,20 +3,22 @@
 # (c) 2020, Ava Labs, Inc. All rights reserved.
 # See the file LICENSE for licensing terms.
 
-# Build the image
+# Build the image with a canonical commit-based name
 IMAGE_NAME="$DOCKER_REGISTRY/ortelius:dev-$(git rev-parse --short HEAD)"
-DOCKER_IMAGE_NAME=IMAGE_NAME make image
+DOCKER_IMAGE_NAME="$IMAGE_NAME" make image
 
-# Install AWS CLI and login to ECR
-which aws || pip install --user awscli
-PATH=$PATH:$HOME/.local/bin aws ecr get-login-password --region=$AWS_REGION | docker login --username $DOCKER_USER --password-stdin $DOCKER_REGISTRY/ortelius
+# Login to docker registry
+echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USER --password-stdin
 
 # Create and publish all the appropriate tags
+# Every commit gets a commit-based tag. Git tags, master branch, and staging
+# branch all get special tags
 tag_and_push () {
   docker tag $IMAGE_NAME "$DOCKER_REGISTRY/ortelius:$1"
   docker push "$DOCKER_REGISTRY/ortelius:$1"
 }
 
+docker push "$IMAGE_NAME"
 if [ "$TRAVIS_TAG" != "" ] ; then tag_and_push $TRAVIS_TAG; fi
 if [ "$TRAVIS_BRANCH" == "master" ] ; then tag_and_push "latest"; fi
 if [ "$TRAVIS_BRANCH" == "staging" ] ; then tag_and_push "staging"; fi
