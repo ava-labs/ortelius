c50b55967b8989eab15104eef6375fb41bf384f0
Tyler Smith <tylersmith.me@gmail.com>
2020-09-17 02:49:19 -0700

    Revert "Revert "TWEAK: Allow parsing large genesis transactions.""
    This reverts commit 8a9c7a3a15744a924d1a5acebb29fc23b373f866.

--- services/indexes/avm/index.go
+++ services/indexes/avm/index.go
@@ -6,6 +6,7 @@ package avm
 import (
 	"context"
 	"errors"
+	"math"
 
 	"github.com/ava-labs/avalanchego/database"
 	"github.com/ava-labs/avalanchego/database/nodb"
@@ -75,7 +76,7 @@ func (i *Index) Bootstrap(ctx context.Context) error {
 	}
 
 	platformGenesis := &platformvm.Genesis{}
-	if err = platformvm.Codec.Unmarshal(platformGenesisBytes, platformGenesis); err != nil {
+	if err = platformvm.GenesisCodec.Unmarshal(platformGenesisBytes, platformGenesis); err != nil {
 		return err
 	}
 	if err = platformGenesis.Initialize(); err != nil {
@@ -251,6 +252,9 @@ func newAVM(chainID ids.ID, networkID uint32) (*avm.VM, error) {
 	if err != nil && err != database.ErrClosed {
 		return nil, err
 	}
+
+	vm.Codec().SetMaxSize(math.MaxUint32)
+	vm.Codec().SetMaxSliceLen(math.MaxUint32)
 
 	return vm, nil
 }
