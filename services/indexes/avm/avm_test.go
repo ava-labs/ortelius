6862c0bd026c6aa6ccde706c87d604f9de14aa2a
Todd Stock <todd@avalabs.org>
2020-10-28 14:40:34 -0400

    fix build

--- services/indexes/avm/avm_test.go
+++ services/indexes/avm/avm_test.go
@@ -46,8 +46,7 @@ func TestIndexBootstrap(t *testing.T) {
 	txList, err = reader.ListTransactions(context.Background(), &params.ListTransactionsParams{
 		ChainIDs:  []string{testXChainID.String()},
 		Addresses: []ids.ShortID{ids.ShortEmpty},
-		AssetID:   &ids.Empty,
-	})
+	}, ids.Empty)
 
 	if err != nil {
 		t.Fatal("Failed to list transactions:", err.Error())
