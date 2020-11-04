ae67cef2e9e65c332a589605b8d05c42d7b3e078
Todd Stock <todd@avalabs.org>
2020-11-04 18:35:29 -0500

    clean comments

--- stream/indexer_task_test.go
+++ stream/indexer_task_test.go
@@ -235,8 +235,6 @@ func TestReplaceAvmAggregate(t *testing.T) {
 
 	pastime := time.Now().Add(-5 * time.Hour).Round(1 * time.Minute).Add(1 * time.Second)
 
-	// aggregationTime := pastime.Truncate(timestampRollup)
-
 	initData(ctx, sess, pastime, t)
 
 	agg := models.AvmAggregate{
@@ -303,8 +301,6 @@ func TestReplaceAvmAggregateCount(t *testing.T) {
 	sess, _ := co.DB().NewSession("producertask", 5*time.Second)
 
 	pastime := time.Now().Add(-5 * time.Hour).Round(1 * time.Minute).Add(1 * time.Second)
-
-	// aggregationTime := pastime.Truncate(timestampRollup)
 
 	initData(ctx, sess, pastime, t)
 
