e47054ec940ddd4cede3bffa22738b6cb04b005d
Todd Stock <todd@avalabs.org>
2020-11-11 07:41:10 -0500

    reset default

--- services/db/migrations/014_locktime_and_redeem.down.sql
+++ services/db/migrations/014_locktime_and_redeem.down.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `avm_outputs` DROP COLUMN locktime;
-ALTER TABLE `avm_outputs` ADD COLUMN locktime int unsigned not null;
+ALTER TABLE `avm_outputs` ADD COLUMN locktime int unsigned default 0 not null;
 ALTER TABLE `avm_outputs` DROP COLUMN stake_locktime;
 ALTER TABLE `avm_outputs` ADD COLUMN redeemed_at timestamp null;
 ALTER TABLE `avm_outputs` ADD COLUMN redeeming_transaction_id varchar(50) default '' not null;
