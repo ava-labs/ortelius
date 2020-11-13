5cecb6bf4a68da866235ddb5333a3fa1dbc6456a
Todd Stock <todd@avalabs.org>
2020-11-13 16:48:29 -0500

    remove pvm tables

--- services/db/migrations/014_locktime_and_redeem.up.sql
+++ services/db/migrations/014_locktime_and_redeem.up.sql
@@ -3,3 +3,11 @@ ALTER TABLE `avm_outputs` ADD COLUMN stake_locktime bigint unsigned not null def
 ALTER TABLE `avm_outputs` DROP COLUMN redeemed_at;
 ALTER TABLE `avm_outputs` DROP COLUMN redeeming_transaction_id;
 drop index avm_outputs_chain_id_redeeming_transaction_id ON avm_outputs;
+
+drop table pvm_transactions;
+drop table pvm_subnets;
+drop table pvm_subnet_control_keys;
+drop table pvm_validators;
+drop table pvm_chains;
+drop table pvm_chains_control_signatures;
+drop table pvm_chains_fx_ids;
