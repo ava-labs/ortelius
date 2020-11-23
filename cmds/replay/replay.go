506c429bf74ba59e766673870ea726f29456c0d6
Todd Stock <todd@avalabs.org>
2020-11-23 18:19:33 -0500

    fix

--- cmds/replay/replay.go
+++ cmds/replay/replay.go
@@ -6,8 +6,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/ava-labs/avalanchego/ids"
@@ -30,86 +30,11 @@ type DBHolder struct {
 	Lock sync.Mutex
 }
 
-type Replay struct {
-	Config    *string
-	GroupName *string
-	DedupDB   *string
-	DBHolder  *DBHolder
-	errs      *utils.AtomicInterface
-	running   *utils.AtomicBool
-	c         *cfg.Config
-}
+func (replay *DBHolder) init() error {
+	replay.Lock.Lock()
+	defer replay.Lock.Unlock()
 
-func main() {
-	cfg.PerformUpdates = true
-
-	config := flag.String("config", "", "config file")
-	groupName := flag.String("group", "", "group name")
-	dedupDB := flag.String("dedupDb", "dedupDb.db", "dedupDb")
-
-	flag.Parse()
-
-	replay := &Replay{
-		Config:    config,
-		GroupName: groupName,
-		DedupDB:   dedupDB,
-	}
-	replay.Start()
-}
-
-func (replay *Replay) Start() {
-	c, err := cfg.NewFromFile(*replay.Config)
-	if err != nil {
-		log.Fatalln("config file not found", replay.Config, ":", err.Error())
-		return
-	}
-
-	replay.c = c
-
-	_, err = logging.New(c.Logging)
-	if err != nil {
-		log.Fatalln("Failed to create log", c.Logging.Directory, ":", err.Error())
-		return
-	}
-
-	db, err := sql.Open("sqlite3", *replay.DedupDB)
-	if err != nil {
-		log.Fatalln("config file not found", replay.Config, ":", err.Error())
-		return
-	}
-
-	replay.DBHolder = &DBHolder{DB: db}
-
-	err = replay.createTable()
-	if err != nil {
-		log.Fatalln("create dedup table failed", ":", err.Error())
-		return
-	}
-
-	replay.errs = &utils.AtomicInterface{}
-	replay.running = &utils.AtomicBool{}
-	replay.running.SetValue(true)
-
-	for _, chainID := range c.Chains {
-		err = replay.handleReader(chainID)
-		if err != nil {
-			log.Fatalln("reader failed", chainID, ":", err.Error())
-			return
-		}
-	}
-
-	for replay.running.GetValue() {
-		time.Sleep(time.Second)
-	}
-
-	fmt.Fprintf(os.Stderr, "err %v\n", replay.errs.GetValue())
-}
-
-func (replay *Replay) createTable() error {
-	replay.DBHolder.Lock.Lock()
-	defer replay.DBHolder.Lock.Unlock()
-
-	statement, err := replay.DBHolder.DB.Prepare("CREATE TABLE IF NOT EXISTS " +
+	statement, err := replay.DB.Prepare("CREATE TABLE IF NOT EXISTS " +
 		"dedup( " +
 		"id VARCHAR NOT NULL, " +
 		"primary key (id) " +
@@ -122,11 +47,11 @@ func (replay *Replay) createTable() error {
 	return err
 }
 
-func (replay *Replay) idPresent(id string) (bool, error) {
-	replay.DBHolder.Lock.Lock()
-	defer replay.DBHolder.Lock.Unlock()
+func (replay *DBHolder) get(id string) (bool, error) {
+	replay.Lock.Lock()
+	defer replay.Lock.Unlock()
 
-	statement, err := replay.DBHolder.DB.Prepare("select id from dedup")
+	statement, err := replay.DB.Prepare("select id from dedup where id=?")
 	if err != nil {
 		return false, err
 	}
@@ -153,11 +78,11 @@ func (replay *Replay) idPresent(id string) (bool, error) {
 	return found, nil
 }
 
-func (replay *Replay) idProcessed(id string) error {
-	replay.DBHolder.Lock.Lock()
-	defer replay.DBHolder.Lock.Unlock()
+func (replay *DBHolder) put(id string) error {
+	replay.Lock.Lock()
+	defer replay.Lock.Unlock()
 
-	statement, err := replay.DBHolder.DB.Prepare("insert into dedup (id) values (?)")
+	statement, err := replay.DB.Prepare("insert into dedup (id) values (?)")
 	if err != nil {
 		return err
 	}
@@ -167,6 +92,157 @@ func (replay *Replay) idProcessed(id string) error {
 		return err
 	}
 	return nil
+}
+
+type Counter struct {
+	countersLock sync.RWMutex
+	counters     map[string]*uint64
+}
+
+func NewCounter() *Counter {
+	return &Counter{counters: make(map[string]*uint64)}
+}
+
+func (c *Counter) Inc(v string) {
+	found := false
+	c.countersLock.RLock()
+	if counter, ok := c.counters[v]; ok {
+		atomic.AddUint64(counter, 1)
+		found = true
+	}
+	c.countersLock.RUnlock()
+
+	if found {
+		return
+	}
+
+	c.countersLock.Lock()
+	if _, ok := c.counters[v]; !ok {
+		c.counters[v] = new(uint64)
+	}
+	atomic.AddUint64(c.counters[v], 1)
+	c.countersLock.Unlock()
+}
+
+func (c *Counter) Clone() map[string]uint64 {
+	countersValues := make(map[string]uint64)
+	c.countersLock.RLock()
+	for cnter := range c.counters {
+		cv := atomic.LoadUint64(c.counters[cnter])
+		if cv != 0 {
+			countersValues[cnter] = cv
+		}
+	}
+	c.countersLock.RUnlock()
+	return countersValues
+}
+
+type Replay struct {
+	ConfigFile *string
+	GroupName  *string
+	DedupDB    *string
+	DBInst     *DBHolder
+	errs       *utils.AtomicInterface
+	running    *utils.AtomicBool
+	Config     *cfg.Config
+
+	CounterRead  *Counter
+	CounterAdded *Counter
+}
+
+func main() {
+	cfg.PerformUpdates = true
+
+	config := flag.String("config", "", "config file")
+	groupName := flag.String("groupName", "", "group name")
+	dedupDB := flag.String("dedupDb", "dedupDb.db", "dedupDb")
+
+	flag.Parse()
+
+	replay := &Replay{
+		ConfigFile:   config,
+		GroupName:    groupName,
+		DedupDB:      dedupDB,
+		CounterRead:  NewCounter(),
+		CounterAdded: NewCounter(),
+	}
+	replay.Start()
+}
+
+func (replay *Replay) Start() {
+	config, err := cfg.NewFromFile(*replay.ConfigFile)
+	if err != nil {
+		log.Fatalln("config file not found", replay.ConfigFile, ":", err.Error())
+		return
+	}
+
+	replay.Config = config
+
+	var alog *logging.Log
+	alog, err = logging.New(config.Logging)
+	if err != nil {
+		log.Fatalln("Failed to create log", config.Logging.Directory, ":", err.Error())
+		return
+	}
+
+	db, err := sql.Open("sqlite3", *replay.DedupDB)
+	if err != nil {
+		log.Fatalln("config file not found", replay.ConfigFile, ":", err.Error())
+		return
+	}
+
+	replay.DBInst = &DBHolder{DB: db}
+
+	err = replay.DBInst.init()
+	if err != nil {
+		log.Fatalln("create dedup table failed", ":", err.Error())
+		return
+	}
+
+	replay.errs = &utils.AtomicInterface{}
+	replay.running = &utils.AtomicBool{}
+	replay.running.SetValue(true)
+
+	for _, chainID := range config.Chains {
+		err = replay.handleReader(chainID)
+		if err != nil {
+			log.Fatalln("reader failed", chainID, ":", err.Error())
+			return
+		}
+	}
+
+	for replay.running.GetValue() {
+		type CounterValues struct {
+			Read  uint64
+			Added uint64
+		}
+
+		ctot := make(map[string]*CounterValues)
+		countersValues := replay.CounterRead.Clone()
+		for cnter := range countersValues {
+			if _, ok := ctot[cnter]; !ok {
+				ctot[cnter] = &CounterValues{}
+			}
+			ctot[cnter].Read = countersValues[cnter]
+		}
+		countersValues = replay.CounterAdded.Clone()
+		for cnter := range countersValues {
+			if _, ok := ctot[cnter]; !ok {
+				ctot[cnter] = &CounterValues{}
+			}
+			ctot[cnter].Added = countersValues[cnter]
+		}
+
+		for cnter := range ctot {
+			alog.Info("%s %d %d", cnter, ctot[cnter].Read, ctot[cnter].Added)
+		}
+
+		time.Sleep(5 * time.Second)
+	}
+
+	if replay.errs.GetValue() != nil {
+		alog.Info("err %v", replay.errs.GetValue())
+	}
 }
 
 type MessageR struct {
@@ -182,7 +258,7 @@ func (m *MessageR) Body() []byte     { return m.body }
 func (m *MessageR) Timestamp() int64 { return m.timestamp }
 
 func (replay *Replay) handleReader(chain cfg.Chain) error {
-	conns, err := services.NewConnectionsFromConfig(replay.c.Services, false)
+	conns, err := services.NewConnectionsFromConfig(replay.Config.Services, false)
 	if err != nil {
 		return err
 	}
@@ -190,12 +266,12 @@ func (replay *Replay) handleReader(chain cfg.Chain) error {
 	var writer services.Consumer
 	switch chain.VMType {
 	case consumers.IndexerAVMName:
-		writer, err = avm.NewWriter(conns, replay.c.NetworkID, chain.ID)
+		writer, err = avm.NewWriter(conns, replay.Config.NetworkID, chain.ID)
 		if err != nil {
 			return err
 		}
 	case consumers.IndexerPVMName:
-		writer, err = pvm.NewWriter(conns, replay.c.NetworkID, chain.ID)
+		writer, err = pvm.NewWriter(conns, replay.Config.NetworkID, chain.ID)
 		if err != nil {
 			return err
 		}
@@ -203,12 +279,14 @@ func (replay *Replay) handleReader(chain cfg.Chain) error {
 		return fmt.Errorf("unknown vmtype")
 	}
 
+	tn := stream.GetTopicName(replay.Config.NetworkID, chain.ID, stream.ConsumerEventTypeDefault)
+
 	go func() {
 		defer replay.running.SetValue(false)
 
 		reader := kafka.NewReader(kafka.ReaderConfig{
-			Topic:       stream.GetTopicName(replay.c.NetworkID, chain.ID, stream.ConsumerEventTypeDefault),
-			Brokers:     replay.c.Kafka.Brokers,
+			Topic:       tn,
+			Brokers:     replay.Config.Kafka.Brokers,
 			GroupID:     *replay.GroupName,
 			StartOffset: kafka.FirstOffset,
 			MaxBytes:    stream.ConsumerMaxBytesDefault,
@@ -223,13 +301,15 @@ func (replay *Replay) handleReader(chain cfg.Chain) error {
 				return
 			}
 
+			replay.CounterRead.Inc(tn)
+
 			id, err := ids.ToID(msg.Key)
 			if err != nil {
 				replay.errs.SetValue(err)
 				return
 			}
 
-			present, err := replay.idPresent(id.String())
+			present, err := replay.DBInst.get(id.String())
 			if err != nil {
 				replay.errs.SetValue(err)
 				return
@@ -237,6 +317,8 @@ func (replay *Replay) handleReader(chain cfg.Chain) error {
 			if present {
 				continue
 			}
+
+			replay.CounterAdded.Inc(tn)
 
 			msgc := &MessageR{
 				chainID:   chain.ID,
@@ -251,7 +333,7 @@ func (replay *Replay) handleReader(chain cfg.Chain) error {
 				return
 			}
 
-			err = replay.idProcessed(id.String())
+			err = replay.DBInst.put(id.String())
 			if err != nil {
 				replay.errs.SetValue(err)
 				return
