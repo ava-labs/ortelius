e61c438a932e43a024ab203a0fdccf28b1ea3ed1
Todd Stock <todd@avalabs.org>
2020-11-26 06:19:30 -0500

    add asc transaction idx fix

--- services/db/migrations/017_outputs_and_blocks_index.down.sql
+++ services/db/migrations/017_outputs_and_blocks_index.down.sql
@@ -1,3 +1,5 @@
 create index `avm_outputs_asset_id` ON `avm_outputs` (asset_id);
 drop index `avm_outputs_asset_id_output_type` on `avm_outputs`;
 drop index `pvm_blocks_parent_id` on `pvm_blocks`;
+DROP INDEX `avm_transactions_chain_id_created_at_asc` ON `avm_transactions`;
+
--- services/db/migrations/017_outputs_and_blocks_index.up.sql
+++ services/db/migrations/017_outputs_and_blocks_index.up.sql
@@ -1,3 +1,5 @@
 create index `avm_outputs_asset_id_output_type` ON `avm_outputs` (asset_id,output_type);
 drop index `avm_outputs_asset_id` on `avm_outputs`;
 create index `pvm_blocks_parent_id` on `pvm_blocks` (parent_id);
+CREATE INDEX `avm_transactions_chain_id_created_at_asc` ON `avm_transactions` (chain_id, created_at asc);
+
