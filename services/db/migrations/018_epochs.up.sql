3be42fdb96a188807cbd9f8c655f2273b4ff22a9
Todd Stock <todd@avalabs.org>
2020-11-30 18:09:07 -0500

    fix eol

--- services/db/migrations/018_epochs.up.sql
+++ services/db/migrations/018_epochs.up.sql
@@ -3,4 +3,4 @@ create table `transaction_epoch`
     id         varchar(50)     not null primary key,
     epoch      bigint unsigned default 0,
     created_at timestamp       not null default current_timestamp
-)
+);
