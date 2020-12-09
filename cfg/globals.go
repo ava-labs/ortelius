0b82ee90180389dac75954570e5c9084027806d8
Todd Stock <todd@avalabs.org>
2020-12-09 07:17:43 -0500

    tune

--- cfg/globals.go
+++ cfg/globals.go
@@ -4,9 +4,9 @@ import "time"
 
 const (
 	// RequestTimeout is the maximum duration to allow an API request to execute
-	RequestTimeout   = 5 * time.Second
-	HTTPWriteTimeout = 30 * time.Second
-	CacheTimeout     = 5 * time.Second
+	RequestTimeout   = 4 * time.Second
+	HTTPWriteTimeout = 20 * time.Second
+	CacheTimeout     = 3 * time.Second
 )
 
 // PerformUpdates controls for performing sql update operations.  Disabled by normal operation.
