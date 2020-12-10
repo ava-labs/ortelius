ee339b79cb2a75aa5b59240f01e952b77ee3e41a
Todd Stock <todd@avalabs.org>
2020-12-10 18:14:25 -0500

    re-work sql

--- services/db/migrations/019_cvm.up.sql
+++ services/db/migrations/019_cvm.up.sql
@@ -7,6 +7,7 @@ create table `cvm_blocks`
 create table `cvm_transactions`
 (
     id             varchar(50)     not null primary key,
+    type           smallint        not null,
     blockchain_id  varchar(50)     not null,
     block          decimal(65)     not null,
     created_at                     timestamp       not null default current_timestamp
@@ -14,16 +15,15 @@ create table `cvm_transactions`
 
 create table `cvm_addresses`
 (
+    id             varchar(50)     not null primary key,
     type           smallint        not null,
-    id             varchar(50)     not null,
     idx            int unsigned    not null,
     transaction_id varchar(50)     not null,
     address        varchar(50)     not null,
     asset_id       varchar(50)     not null,
     amount         bigint unsigned not null default 0,
     nonce          bigint unsigned not null default 0,
-    created_at     timestamp       not null default current_timestamp,
-    primary key (type, id)
+    created_at     timestamp       not null default current_timestamp
 );
 
 create index cvm_address_transaction_id ON cvm_addresses (transaction_id);
