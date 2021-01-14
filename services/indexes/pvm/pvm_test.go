9fdba923f848e4bc397832d50bba61640b7bdc83
tasinco <67973730+tasinco@users.noreply.github.com>
2021-01-14 07:59:55 -0500

    Combine readers (#281)

--- services/indexes/pvm/pvm_test.go
+++ services/indexes/pvm/pvm_test.go
@@ -85,7 +85,7 @@ func newTestIndex(t *testing.T, networkID uint32, chainID ids.ID) (*Writer, *ava
 		t.Fatal("Failed to create writer:", err.Error())
 	}
 
-	reader := avax.NewReader(conns, nil)
+	reader := avax.NewReader(conns)
 	return writer, reader, func() {
 		s.Close()
 		_ = conns.Close()
