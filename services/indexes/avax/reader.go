b5e7920b2a266eda08794c157a73fac6af81d0d8
Todd Stock <todd@avalabs.org>
2021-01-28 15:17:03 -0500

    upd

--- services/indexes/avax/reader.go
+++ services/indexes/avax/reader.go
@@ -756,35 +756,6 @@ func (r *Reader) GetAddress(ctx context.Context, p *params.ListAddressesParams) 
 	return nil, err
 }
 
-func addAssetInfoMap(assets map[models.StringID]models.AssetInfo, assets2 map[models.StringID]models.AssetInfo) map[models.StringID]models.AssetInfo {
-	for k, v := range assets2 {
-		if assetInfo, ok := assets[k]; ok {
-			assets[k] = addAssetInfo(assetInfo, v)
-		} else {
-			assets[k] = v
-		}
-	}
-	return assets
-}
-
-func addStringaAsInt(t string, f string) string {
-	tbi := new(big.Int)
-	tbi.SetString(t, 10)
-	fbi := new(big.Int)
-	fbi.SetString(f, 10)
-	rbi := new(big.Int)
-	return rbi.Add(tbi, fbi).String()
-}
-
-func addAssetInfo(t models.AssetInfo, f models.AssetInfo) models.AssetInfo {
-	t.TransactionCount += f.TransactionCount
-	t.UTXOCount += t.UTXOCount
-	t.Balance = models.TokenAmount(addStringaAsInt(string(t.Balance), string(f.Balance)))
-	t.TotalReceived = models.TokenAmount(addStringaAsInt(string(t.TotalReceived), string(f.TotalReceived)))
-	t.TotalSent = models.TokenAmount(addStringaAsInt(string(t.TotalSent), string(f.TotalSent)))
-	return t
-}
-
 func (r *Reader) GetOutput(ctx context.Context, id ids.ID) (*models.Output, error) {
 	outputList, err := r.ListOutputs(ctx,
 		&params.ListOutputsParams{
