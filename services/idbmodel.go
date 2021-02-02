cf5cf34023fc211c0c72cac93ed199af5ca39842
Todd Stock <todd@avalabs.org>
2021-02-02 06:42:52 -0500

    fix insert

--- services/idbmodel.go
+++ services/idbmodel.go
@@ -895,7 +895,7 @@ func (p *persist) InsertCvmTransactions(
 ) error {
 	var err error
 	_, err = sess.
-		InsertBySql("insert into "+TableCvmTransactions+" (id,transaction_id,type,blockchain_id,created_at,block,serialization) values(?,?,?,?,"+v.Block+",?)",
+		InsertBySql("insert into "+TableCvmTransactions+" (id,transaction_id,type,blockchain_id,created_at,block,serialization) values(?,?,?,?,?,"+v.Block+",?)",
 			v.ID, v.TransactionID, v.Type, v.BlockchainID, v.CreatedAt, v.Serialization).
 		ExecContext(ctx)
 	if err != nil && !db.ErrIsDuplicateEntryError(err) {
