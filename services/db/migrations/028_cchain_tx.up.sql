c9b3bcba575f3c43a674bae29258262169bac741
Todd Stock <todd@avalabs.org>
2021-02-09 09:42:11 -0500

    clean db upd

--- services/db/migrations/028_cchain_tx.up.sql
+++ services/db/migrations/028_cchain_tx.up.sql
@@ -1,16 +1,16 @@
 create table `cvm_transactions_txdata`
 (
-    hash           varchar(100)    not null,
     block          decimal(65)     not null,
     idx            bigint unsigned not null,
+    hash           varchar(100)    not null,
     rcpt           varchar(50)     not null,
     nonce          bigint unsigned not null,
     serialization  mediumblob,
     created_at     timestamp(6)       not null default current_timestamp(6),
-    primary key(hash)
+    primary key(block,idx)
 );
 
-create index cvm_transactions_txdata_block ON cvm_transactions_txdata (block, idx);
+create index cvm_transactions_txdata_hash ON cvm_transactions_txdata (hash);
 create index cvm_transactions_txdata_rcpt ON cvm_transactions_txdata (rcpt);
 
 alter table `cvm_transactions` add COLUMN `tx_time` timestamp(6) not null default current_timestamp(6);
