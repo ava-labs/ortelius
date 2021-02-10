d0d3fa6e12386d900c6bd207af38e2347b28ad80
Todd Stock <todd@avalabs.org>
2021-02-10 10:32:20 -0500

    index ouptut_id

--- services/db/migrations/030_accum_fix.up.sql
+++ services/db/migrations/030_accum_fix.up.sql
@@ -1,3 +1,4 @@
 alter table `output_addresses_accumulate_in` add COLUMN `output_processed` smallint unsigned not null default 0;
 create index `output_addresses_accumulate_in_output_processed` on `output_addresses_accumulate_in` (output_processed asc, processed asc, created_at asc);
 create index `output_addresses_accumulate_in_output_id` on `output_addresses_accumulate_in` (output_id asc);
+create index `output_addresses_accumulate_out_output_id` on `output_addresses_accumulate_out` (output_id asc);
