7a8a6ecad011c395cc7222ce1c4ca27e2366c126
Todd Stock <todd@avalabs.org>
2021-02-16 15:04:55 -0500

    fix build

--- services/idbmodel_mock.go
+++ services/idbmodel_mock.go
@@ -485,11 +485,11 @@ func (m *MockPersist) InsertTxPool(ctx context.Context, runner dbr.SessionRunner
 	return nil
 }
 
-func (m *MockPersist) UpdateTxPoolStatus(ctx context.Context, runner dbr.SessionRunner, v *TxPool) (*TxPool, error) {
+func (m *MockPersist) UpdateTxPoolStatus(ctx context.Context, runner dbr.SessionRunner, v *TxPool) error {
 	m.lock.RLock()
 	defer m.lock.RUnlock()
 	if fv, present := m.TxPool[v.ID]; present {
 		fv.Processed = v.Processed
 	}
-	return nil, nil
+	return nil
 }
