18af4dbdbf9b036e6d74229326895fb1f9a10769
Todd Stock <todd@avalabs.org>
2021-02-26 10:12:13 -0500

    Revert: set max age

--- api/utils.go
+++ api/utils.go
@@ -22,7 +22,6 @@ type ErrorResponse struct {
 
 // WriteJSON writes the given bytes to the http response as JSON
 func WriteJSON(w http.ResponseWriter, msg []byte) {
-	w.Header().Add("Cache-Control", "max-age=5")
 	w.WriteHeader(200)
 	_, _ = w.Write(msg)
 }
