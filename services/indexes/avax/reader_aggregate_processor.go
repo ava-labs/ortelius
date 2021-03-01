fdae403e556989d4242f430bb763cef414aac783
Todd Stock <todd@avalabs.org>
2021-03-01 13:34:32 -0500

    refactor

--- services/indexes/avax/reader_aggregate_processor.go
+++ services/indexes/avax/reader_aggregate_processor.go
@@ -18,22 +18,26 @@ func (r *Reader) aggregateProcessor1h(conns *services.Connections) {
 
 	time1h := time.Now().Truncate(time.Minute)
 
-	ctx := context.Background()
+	runAgg := func() {
+		ctx := context.Background()
+		p := &params.AggregateParams{}
+		urlv := url.Values{}
+		urlv.Add(params.KeyIntervalSize, "hour")
+		err := p.ForValues(1, urlv)
+		if err == nil {
+			p.ListParams.EndTime = time.Now().Truncate(time.Minute)
+			p.ListParams.StartTime = p.ListParams.EndTime.Add(-time.Hour)
+			p.ChainIDs = append(p.ChainIDs, r.sc.GenesisContainer.XChainID.String())
+			_, _ = r.Aggregate(ctx, p, conns)
+		}
+	}
+	runAgg()
 	for {
 		select {
 		case <-ticker.C:
 			tnow := time.Now()
 			if tnow.After(time1h) {
-				p := &params.AggregateParams{}
-				urlv := url.Values{}
-				urlv.Add(params.KeyIntervalSize, "hour")
-				err := p.ForValues(1, urlv)
-				if err == nil {
-					p.ListParams.EndTime = time.Now().Truncate(time.Minute)
-					p.ListParams.StartTime = p.ListParams.EndTime.Add(-time.Hour)
-					p.ChainIDs = append(p.ChainIDs, r.sc.GenesisContainer.XChainID.String())
-					_, _ = r.Aggregate(ctx, p, conns)
-				}
+				runAgg()
 				time1h = time1h.Add(5 * time.Minute)
 			}
 		case <-r.doneCh:
@@ -51,22 +55,26 @@ func (r *Reader) aggregateProcessor24h(conns *services.Connections) {
 
 	time24h := time.Now().Truncate(time.Minute)
 
-	ctx := context.Background()
+	runAgg := func() {
+		ctx := context.Background()
+		p := &params.AggregateParams{}
+		urlv := url.Values{}
+		urlv.Add(params.KeyIntervalSize, "day")
+		err := p.ForValues(1, urlv)
+		if err == nil {
+			p.ListParams.EndTime = time.Now().Truncate(time.Minute)
+			p.ListParams.StartTime = p.ListParams.EndTime.Add(-(24 * time.Hour))
+			p.ChainIDs = append(p.ChainIDs, r.sc.GenesisContainer.XChainID.String())
+			_, _ = r.Aggregate(ctx, p, conns)
+		}
+	}
+	runAgg()
 	for {
 		select {
 		case <-ticker.C:
 			tnow := time.Now()
 			if tnow.After(time24h) {
-				p := &params.AggregateParams{}
-				urlv := url.Values{}
-				urlv.Add(params.KeyIntervalSize, "day")
-				err := p.ForValues(1, urlv)
-				if err == nil {
-					p.ListParams.EndTime = time.Now().Truncate(time.Minute)
-					p.ListParams.StartTime = p.ListParams.EndTime.Add(-(24 * time.Hour))
-					p.ChainIDs = append(p.ChainIDs, r.sc.GenesisContainer.XChainID.String())
-					_, _ = r.Aggregate(ctx, p, conns)
-				}
+				runAgg()
 				time24h = time24h.Add(10 * time.Minute)
 			}
 		case <-r.doneCh:
@@ -84,22 +92,26 @@ func (r *Reader) aggregateProcessor7d(conns *services.Connections) {
 
 	time7d := time.Now().Truncate(time.Minute)
 
-	ctx := context.Background()
+	runAgg := func() {
+		ctx := context.Background()
+		p := &params.AggregateParams{}
+		urlv := url.Values{}
+		urlv.Add(params.KeyIntervalSize, "week")
+		err := p.ForValues(1, urlv)
+		if err == nil {
+			p.ListParams.EndTime = time.Now().Truncate(time.Minute)
+			p.ListParams.StartTime = p.ListParams.EndTime.Add(-(7 * 24 * time.Hour))
+			p.ChainIDs = append(p.ChainIDs, r.sc.GenesisContainer.XChainID.String())
+			_, _ = r.Aggregate(ctx, p, conns)
+		}
+	}
+	runAgg()
 	for {
 		select {
 		case <-ticker.C:
 			tnow := time.Now()
 			if tnow.After(time7d) {
-				p := &params.AggregateParams{}
-				urlv := url.Values{}
-				urlv.Add(params.KeyIntervalSize, "week")
-				err := p.ForValues(1, urlv)
-				if err == nil {
-					p.ListParams.EndTime = time.Now().Truncate(time.Minute)
-					p.ListParams.StartTime = p.ListParams.EndTime.Add(-(7 * 24 * time.Hour))
-					p.ChainIDs = append(p.ChainIDs, r.sc.GenesisContainer.XChainID.String())
-					_, _ = r.Aggregate(ctx, p, conns)
-				}
+				runAgg()
 				time7d = time7d.Add(time.Hour)
 			}
 		case <-r.doneCh:
@@ -117,22 +129,26 @@ func (r *Reader) aggregateProcessor30d(conns *services.Connections) {
 
 	time30d := time.Now().Truncate(time.Minute)
 
-	ctx := context.Background()
+	runAgg := func() {
+		ctx := context.Background()
+		p := &params.AggregateParams{}
+		urlv := url.Values{}
+		urlv.Add(params.KeyIntervalSize, "month")
+		err := p.ForValues(1, urlv)
+		if err == nil {
+			p.ListParams.EndTime = time.Now().Truncate(time.Minute)
+			p.ListParams.StartTime = p.ListParams.EndTime.Add(-(30 * 24 * time.Hour))
+			p.ChainIDs = append(p.ChainIDs, r.sc.GenesisContainer.XChainID.String())
+			_, _ = r.Aggregate(ctx, p, conns)
+		}
+	}
+	runAgg()
 	for {
 		select {
 		case <-ticker.C:
 			tnow := time.Now()
 			if tnow.After(time30d) {
-				p := &params.AggregateParams{}
-				urlv := url.Values{}
-				urlv.Add(params.KeyIntervalSize, "month")
-				err := p.ForValues(1, urlv)
-				if err == nil {
-					p.ListParams.EndTime = time.Now().Truncate(time.Minute)
-					p.ListParams.StartTime = p.ListParams.EndTime.Add(-(30 * 24 * time.Hour))
-					p.ChainIDs = append(p.ChainIDs, r.sc.GenesisContainer.XChainID.String())
-					_, _ = r.Aggregate(ctx, p, conns)
-				}
+				runAgg()
 				time30d = time30d.Add(4 * time.Hour)
 			}
 		case <-r.doneCh:
@@ -150,23 +166,26 @@ func (r *Reader) aggregateProcessor1y(conns *services.Connections) {
 
 	time1y := time.Now().Truncate(time.Minute)
 
-	ctx := context.Background()
-
+	runAgg := func() {
+		ctx := context.Background()
+		p := &params.AggregateParams{}
+		urlv := url.Values{}
+		urlv.Add(params.KeyIntervalSize, "year")
+		err := p.ForValues(1, urlv)
+		if err == nil {
+			p.ListParams.EndTime = time.Now().Truncate(time.Minute)
+			p.ListParams.StartTime = p.ListParams.EndTime.Add(-(365 * 24 * time.Hour))
+			p.ChainIDs = append(p.ChainIDs, r.sc.GenesisContainer.XChainID.String())
+			_, _ = r.Aggregate(ctx, p, conns)
+		}
+	}
+	runAgg()
 	for {
 		select {
 		case <-ticker.C:
 			tnow := time.Now()
 			if tnow.After(time1y) {
-				p := &params.AggregateParams{}
-				urlv := url.Values{}
-				urlv.Add(params.KeyIntervalSize, "year")
-				err := p.ForValues(1, urlv)
-				if err == nil {
-					p.ListParams.EndTime = time.Now().Truncate(time.Minute)
-					p.ListParams.StartTime = p.ListParams.EndTime.Add(-(365 * 24 * time.Hour))
-					p.ChainIDs = append(p.ChainIDs, r.sc.GenesisContainer.XChainID.String())
-					_, _ = r.Aggregate(ctx, p, conns)
-				}
+				runAgg()
 				time1y = time1y.Add(12 * time.Hour)
 			}
 		case <-r.doneCh:
