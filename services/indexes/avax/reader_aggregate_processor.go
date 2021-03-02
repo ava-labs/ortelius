ae42de35e2f5527279fa9c908162f87b7d030878
Todd Stock <todd@avalabs.org>
2021-03-02 15:25:02 -0500

    upd

--- services/indexes/avax/reader_aggregate_processor.go
+++ services/indexes/avax/reader_aggregate_processor.go
@@ -18,7 +18,6 @@ func (r *Reader) aggregateProcessor() error {
 	var connections24h *services.Connections
 	var connections7d *services.Connections
 	var connections30d *services.Connections
-	var connections1y *services.Connections
 	var err error
 
 	closeDBForError := func() {
@@ -33,9 +32,6 @@ func (r *Reader) aggregateProcessor() error {
 		}
 		if connections30d != nil {
 			_ = connections30d.Close()
-		}
-		if connections1y != nil {
-			_ = connections1y.Close()
 		}
 	}
 
@@ -59,17 +55,11 @@ func (r *Reader) aggregateProcessor() error {
 		closeDBForError()
 		return err
 	}
-	connections1y, err = r.sc.DatabaseRO()
-	if err != nil {
-		closeDBForError()
-		return err
-	}
 
 	go r.aggregateProcessor1h(connections1h)
 	go r.aggregateProcessor24h(connections24h)
 	go r.aggregateProcessor7d(connections7d)
 	go r.aggregateProcessor30d(connections30d)
-	go r.aggregateProcessor1y(connections1y)
 	return nil
 }
 
@@ -86,7 +76,7 @@ func (r *Reader) aggregateProcessor1h(conns *services.Connections) {
 		ctx := context.Background()
 		p := &params.AggregateParams{}
 		urlv := url.Values{}
-		urlv.Add(params.KeyIntervalSize, "hour")
+		urlv.Add(params.KeyIntervalSize, "5m")
 		err := p.ForValues(1, urlv)
 		if err != nil {
 			r.sc.Log.Warn("Aggregate %v", err)
@@ -238,49 +228,6 @@ func (r *Reader) aggregateProcessor30d(conns *services.Connections) {
 			if tnow.After(time30d) {
 				runAgg()
 				time30d = time30d.Add(4 * time.Hour)
-			}
-		case <-r.doneCh:
-			return
-		}
-	}
-}
-
-func (r *Reader) aggregateProcessor1y(conns *services.Connections) {
-	defer func() {
-		_ = conns.Close()
-	}()
-
-	ticker := time.NewTicker(1 * time.Minute)
-
-	time1y := time.Now().Truncate(time.Minute)
-
-	runAgg := func() {
-		ctx := context.Background()
-		p := &params.AggregateParams{}
-		urlv := url.Values{}
-		urlv.Add(params.KeyIntervalSize, "year")
-		err := p.ForValues(1, urlv)
-		if err != nil {
-			r.sc.Log.Warn("Aggregate %v", err)
-			return
-		}
-		p.ListParams.EndTime = time.Now().Truncate(time.Minute)
-		p.ListParams.StartTime = p.ListParams.EndTime.Add(-(365 * 24 * time.Hour))
-		p.ChainIDs = append(p.ChainIDs, r.sc.GenesisContainer.XChainID.String())
-		_, err = r.Aggregate(ctx, p, conns)
-		if err != nil {
-			r.sc.Log.Warn("Aggregate %v", err)
-			return
-		}
-	}
-	runAgg()
-	for {
-		select {
-		case <-ticker.C:
-			tnow := time.Now()
-			if tnow.After(time1y) {
-				runAgg()
-				time1y = time1y.Add(12 * time.Hour)
 			}
 		case <-r.doneCh:
 			return
