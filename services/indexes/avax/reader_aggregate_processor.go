33763164dc3600d0eb50b2447b46ce97b4031723
Todd Stock <todd@avalabs.org>
2021-04-07 10:16:19 -0400

    fix sql

--- services/indexes/avax/reader_aggregate_processor.go
+++ services/indexes/avax/reader_aggregate_processor.go
@@ -172,7 +172,7 @@ func (r *Reader) aggregateProcessorAssetAggr(conns *services.Connections) {
 		var addressCountl []*models.AddressCounts
 		_, err = sess.Select(
 			"chain_id",
-			"cast(count(*)) as total",
+			"cast(count(*) as char) as total",
 		).
 			From(services.TableAddressChain).
 			GroupBy("chain_id").
