b216b6f250b14148b24ac8b78b81e202f22e1d4e
Todd Stock <todd@avalabs.org>
2021-04-24 18:34:34 -0400

    upd

--- stream/consumers/indexer.go
+++ stream/consumers/indexer.go
@@ -131,7 +131,7 @@ type IndexerFactoryControl struct {
 	fsm       map[string]stream.ProcessorDB
 	msgChan   chan *IndexerFactoryContainer
 	doneCh    chan struct{}
-	msgChanSz *int64
+	msgChanSz int64
 }
 
 func (c *IndexerFactoryControl) updateTxPollStatus(conns *services.Connections, txPoll *services.TxPool) error {
@@ -149,7 +149,7 @@ func (c *IndexerFactoryControl) handleTxPool(conns *services.Connections) {
 	for {
 		select {
 		case txd := <-c.msgChan:
-			atomic.AddInt64(c.msgChanSz, -1)
+			atomic.AddInt64(&c.msgChanSz, -1)
 			if c.sc.SizedList.Exists(txd.txPool.ID) {
 				continue
 			}
@@ -189,11 +189,10 @@ func IndexerFactories(
 	runningControl utils.Running,
 ) error {
 	ctrl := &IndexerFactoryControl{
-		sc:        sc,
-		msgChanSz: new(int64),
-		fsm:       make(map[string]stream.ProcessorDB),
-		msgChan:   make(chan *IndexerFactoryContainer, cfg.MaxIndexerChanSize),
-		doneCh:    make(chan struct{}),
+		sc:      sc,
+		fsm:     make(map[string]stream.ProcessorDB),
+		msgChan: make(chan *IndexerFactoryContainer, cfg.MaxIndexerChanSize),
+		doneCh:  make(chan struct{}),
 	}
 
 	var topicNames []string
@@ -234,7 +233,7 @@ func IndexerFactories(
 
 	enqueueMsgChan := func(txPool *services.TxPool, errs *avlancheGoUtils.AtomicInterface) {
 		ctrl.msgChan <- &IndexerFactoryContainer{txPool: txPool, errs: errs}
-		atomic.AddInt64(ctrl.msgChanSz, 1)
+		atomic.AddInt64(&ctrl.msgChanSz, 1)
 	}
 
 	for ipos := 0; ipos < MaxTheads; ipos++ {
@@ -247,11 +246,6 @@ func IndexerFactories(
 		go ctrl.handleTxPool(conns1)
 	}
 
-	quitCh := make(chan struct{})
-	defer func() {
-		close(quitCh)
-	}()
-
 	wg.Add(1)
 	go func() {
 		wg.Done()
@@ -259,7 +253,7 @@ func IndexerFactories(
 			select {
 			case txPool := <-sc.LocalTxPool:
 				enqueueMsgChan(txPool, nil)
-			case <-quitCh:
+			case <-ctrl.doneCh:
 				return
 			}
 		}
@@ -330,7 +324,7 @@ func IndexerFactories(
 				enqueueMsgChan(txp, errs)
 			}
 
-			for atomic.LoadInt64(ctrl.msgChanSz) > 0 {
+			for atomic.LoadInt64(&ctrl.msgChanSz) > 0 {
 				time.Sleep(1 * time.Millisecond)
 			}
 
