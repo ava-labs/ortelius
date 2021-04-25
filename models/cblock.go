39a0fe52423d8ed3a57ca71c19e31ce0ade66eda
Todd Stock <todd@avalabs.org>
2021-04-25 05:58:21 -0400

    revert

--- models/cblock.go
+++ models/cblock.go
@@ -139,9 +139,9 @@ type TracerParam struct {
 }
 
 type BlockContainer struct {
-	Block  *types.Block        `json:"block"`
-	Traces []*TransactionTrace `json:"traces"`
-	Logs   []*types.Log        `json:"logs"`
+	Block  *types.Block
+	Traces []*TransactionTrace
+	Logs   []*types.Log
 }
 
 func (c *Client) ReadBlock(blockNumber *big.Int, rpcTimeout time.Duration) (*BlockContainer, error) {
