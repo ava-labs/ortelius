2a06747472303ae69795bfd19130f41f20442fa5
Todd Stock <todd@avalabs.org>
2021-05-11 18:17:53 -0400

    lint

--- services/indexes/avax/avm_reader.go
+++ services/indexes/avax/avm_reader.go
@@ -7,13 +7,11 @@ import (
 	"context"
 	"time"
 
-	"github.com/ava-labs/ortelius/services/servicesconn"
-
-	"github.com/ava-labs/ortelius/cfg"
-
 	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/ortelius/cfg"
 	"github.com/ava-labs/ortelius/services/indexes/models"
 	"github.com/ava-labs/ortelius/services/indexes/params"
+	"github.com/ava-labs/ortelius/services/servicesconn"
 	"github.com/gocraft/dbr/v2"
 )
 
