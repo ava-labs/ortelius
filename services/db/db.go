e127909ee08d6fe2c8e536a338120d80e2890fbc
Todd Stock <todd@avalabs.org>
2021-05-13 08:12:50 -0400

    Attempt fix test

--- services/db/db.go
+++ services/db/db.go
@@ -106,7 +106,7 @@ func newDBRConnection(stream *health.Stream, conf cfg.DB, ro bool) (*dbr.Connect
 		return nil, err
 	}
 
-	ctx, cancelFn := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancelFn := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancelFn()
 	if err := rawDBConn.PingContext(ctx); err != nil {
 		rawDBConn.Close()
