d58ec3005839dcf9e7c922560da5a25aa7736280
Todd Stock <todd@avalabs.org>
2021-05-25 11:55:38 -0400

    upd

---
+++ api/delay_cache.go
@@ -0,0 +1,48 @@
+package cache
+
+import (
+	"context"
+	"time"
+
+	"github.com/ava-labs/ortelius/cfg"
+	"github.com/ava-labs/ortelius/utils/worker"
+)
+
+var (
+	workerQueueSize   = 10
+	workerThreadCount = 1
+)
+
+type CacheJob struct {
+	Key  string
+	Body *[]byte
+	TTL  time.Duration
+}
+
+type DelayCache struct {
+	Cache  Cache
+	worker worker.Worker
+}
+
+func NewDelayCache(cache Cache) *DelayCache {
+	c := &DelayCache{Cache: cache}
+	c.worker = worker.NewWorker(workerQueueSize, workerThreadCount, c.Processor)
+	return c
+}
+
+func (c *DelayCache) Enque(j *CacheJob) {
+	if c.worker.JobCnt() < int64(workerQueueSize) {
+		return
+	}
+	c.worker.Enque(j)
+}
+
+func (c *DelayCache) Processor(_ int, job interface{}) {
+	if j, ok := job.(*CacheJob); ok {
+		ctxset, cancelFnSet := context.WithTimeout(context.Background(), cfg.CacheTimeout)
+		defer cancelFnSet()
+
+		// if cache did not set, we can just ignore.
+		_ = c.Cache.Set(ctxset, j.Key, *j.Body, j.TTL)
+	}
+}
